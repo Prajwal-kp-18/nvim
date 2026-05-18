00381c447105e8178827e4e814741811f456bf04
Prajwal <prajwal.kp.1817@gmail.com>
2026-05-18 05:50:50 +0530

    Add configuration files and plugin setup for Neovim

---
+++ lua/config/settings.lua
@@ -0,0 +1,28 @@
+-- number/sign column
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.opt.signcolumn = "yes"
+
+-- cursorline
+vim.opt.cursorline = true
+
+-- line wrap
+vim.opt.wrap = false
+
+-- indent
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+vim.opt.smartindent = true
+
+-- undo
+vim.opt.swapfile = false
+vim.opt.backup = false
+vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
+vim.opt.undofile = true
+
+-- colors
+vim.opt.termguicolors = true
+vim.cmd([[let &t_Cs = "\e[4:3m"]])
+vim.cmd([[let &t_Ce = "\e[4:0m"]])
